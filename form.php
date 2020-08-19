9569f7835b3bb6a98582229343ad734f610ea485
Valentina Tallqvist <valentina@tallqvist.net>
2020-08-19 12:19:39 +0300

    yhteydenotto

--- form.php
+++ form.php
@@ -1,10 +1,18 @@
+<?php
+if($_POST["message"]) {
+    mail("valentina@tallqvist.net", "Yhteydenottolomakevastaus", $_POST["message"], "From: Wikner Ratsutila Yhteydenottolomake");
+}
+?>
 <html>
 <head>
 <link rel="stylesheet" href="/css/style.css">
 <title>Yhteydenottolomake</title>
 </head>
 <body>
-
-
+  <h1>Yhteydenottolomak</h1>
+  <form method="post" action="form.php">
+      <textarea name="message"></textarea>
+      <input type="submit">
+  </form>
 </body>
 </html>
